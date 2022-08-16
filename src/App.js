f9fc4869017a9a16eb979d766f94a171823ffea3
mittwoch01 <wenigwaves@gmail.com>
2022-08-16 17:19:56 +0900

    리액트에서 데이터 바인딩 하는 법

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// App.js는 메인페이지에 들어갈 HTML을 짜는 곳.
+// JSX 문법 사용중 = 소괄호()만 열면 안에 Html문법을 작성할 수 있다. 
 function App() {
+   
+  let posts = '강남 고기 맛집'; // 1) 서버에서 가져온 데이터를
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> 실험.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+       <div>개발 blog</div>
+      </div>
+      <h4>블로그 글</h4> 
     </div>
   );
 }
 
 export default App;
+  

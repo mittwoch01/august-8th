517aae52fd52997db6c210931faf58fe27a04a7b
mittwoch01 <wenigwaves@gmail.com>
2022-08-16 17:39:56 +0900

    image 데이터 바인딩 할 때

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import logo from './logo.svg';
+// *이미지 데이터 바인딩 할 때 
+// 1) src폴더에 이미지를 넣고 
+// 2) import로 불러온다음
+// 3) { 변수명 }을 넣어 사용한다.
 import './App.css';
 
 // App.js는 메인페이지에 들어갈 HTML을 짜는 곳.
@@ -11,16 +16,14 @@ function App() {
   // 2) h4 자리에서 보여줘야 함 > 내가 넣을 자리를 찾아서 { 변수명, 함수 } 을 해준다.
 
   let posts = '강남 고기 맛집'; 
-  function 함수() {
-    return 100
-  }
 
   return (
     <div className="App">
       <div className="black-nav">
        <div>개발 blog</div>
       </div>
-      <h4> { 함수() } </h4> 
+      <img src={ logo } /> 
+      <h4> { posts } </h4> 
     </div>
   );
 }
